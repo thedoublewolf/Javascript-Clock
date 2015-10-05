fb1fd0974c8e7505906743795a453cc42be24f1d
Jonny Warren <jonnywarren06@gmail.com>
2015-10-05 17:31:54 -0400

    working on getting the clock background to change color

--- js/main.js
+++ js/main.js
@@ -1,35 +1,30 @@
-// Target my clock
-var hour = $('.hour');
-var min = $('.min');
-var sec = $('.sec');
+	window.setInterval (function (){
+		var currentHour = new Date().getHours();
+		if (currentHour < 10) {
+		(currentHour) = ('0' + currentHour);
+		} else if (currentHour > 10) {
+			currentHour = currentHour;
+		};
+		var currentMin = new Date().getMinutes();
+		if (currentMin < 10) {
+		(currentMin) = ('0' + currentMin);
+		} else if (currentMin > 10) {
+			currentMin = currentMin;
+		};
+		var currentSec = new Date().getSeconds();
+		if (currentSec < 10) {
+		(currentSec) = ('0' + currentSec);
+		} else if (currentSec > 10) {
+			currentSec = currentSec;
+		};
+	document.getElementById('clock').innerHTML = '#' + currentHour + currentMin + currentSec;
+	}, 1000);
+
+	var hexback = $('#hexback');
 
 	window.setInterval (function (){
-	var currentHour = new Date().getHours();
-	if (currentHour < 10) {
-	(currentHour) = ('0' + currentHour);
-	} else if (currentHour > 10) {
-		currentHour = currentHour;
-	}
-	document.getElementById('hour').innerHTML = currentHour;
-	}, 500);
+		hexback.css('backgroundColor');
+	}, 1000);
 
-	window.setInterval (function (){
-	var currentMin = new Date().getMinutes();
-	if (currentMin < 10) {
-	(currentMin) = ('0' + currentMin);
-	} else if (currentMin > 10) {
-		currentMin = currentMin;
-	}	document.getElementById('min').innerHTML = currentMin;
-	}, 500);
-
-	window.setInterval (function (){
-	var currentSec = new Date().getSeconds();
-	if (currentSec < 10) {
-	(currentSec) = ('0' + currentSec);
-	} else if (currentSec > 10) {
-		currentSec = currentSec;
-	}
-	document.getElementById('sec').innerHTML = currentSec;
-	}, 500);
 
 
